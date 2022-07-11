b41a3560bdf978ffee1d9c5a62286a6353bfebb4
Logan <frenchlogan08@gmail.com>
2022-07-10 23:32:36 -0400

    rdp

--- terraform-work/tf-aws/ec2-open-internet/main.tf
+++ terraform-work/tf-aws/ec2-open-internet/main.tf
@@ -65,6 +65,12 @@ resource "aws_security_group" "web-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   egress {
         from_port   = 0
     to_port     = 0
