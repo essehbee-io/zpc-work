248e8eae9bca9632c20f2fcc2433666275da2fb7
Logan <frenchlogan08@gmail.com>
2022-07-10 21:33:40 -0400

    f

--- terraform-work/tf-aws/ec2-open-internet/main.tf
+++ terraform-work/tf-aws/ec2-open-internet/main.tf
@@ -4,6 +4,9 @@ resource "aws_instance" "webserver" {
 
   ami = "ami-09d56f8956ab235b3"
   instance_type = var.instance_type
+  tags {
+    Name = "tf-ec2-open-internet"
+  }
   key_name = "zscc"
  vpc_security_group_ids = [aws_security_group.web-sg.id]
 associate_public_ip_address = true
